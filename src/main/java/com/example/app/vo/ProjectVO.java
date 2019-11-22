4a97c1a9ee845a507de0a73708438f31d36177b7
cheeze <33196437+Do-yoon@users.noreply.github.com>
2019-11-22 19:14:31 +0900

    removed Date variable

--- src/main/java/com/example/app/vo/ProjectVO.java
+++ src/main/java/com/example/app/vo/ProjectVO.java
@@ -1,13 +1,11 @@
 package com.example.app.vo;
 
-import java.util.Date;
-
 public class ProjectVO {
 
     private Integer id;
     private String name;
-    private Date date1;
-    private Date date2;
+    private String date1;
+    private String date2;
     private String intro;
 
     public int getId() {
@@ -26,19 +24,19 @@ public class ProjectVO {
         this.name = name;
     }
 
-    public Date getDate1() {
+    public String getDate1() {
         return date1;
     }
 
-    public void setDate1(Date date1) {
+    public void setDate1(String date1) {
         this.date1 = date1;
     }
 
-    public Date getDate2() {
+    public String getDate2() {
         return date2;
     }
 
-    public void setDate2(Date date2) {
+    public void setDate2(String date2) {
         this.date2 = date2;
     }
 
